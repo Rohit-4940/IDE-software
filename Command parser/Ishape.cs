a66b0140375cb72bc8a7a693b961d8b6b7e2ba6d
Rohit Chapagain <chapagainrohit4940@gmail.com>
2021-05-26 20:02:03 +0545

    Second commit

---
+++ Command parser/Ishape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_parser
+{
+    interface Ishape
+    {
+ 
+        void draw(Graphics g);
+
+        void set(params int[] list);
+    }
+}
