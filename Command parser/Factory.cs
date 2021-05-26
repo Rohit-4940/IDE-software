ae445cbe45c533054345f7718cfbc0439b8fda08
Rohit Chapagain <chapagainrohit4940@gmail.com>
2021-05-26 20:37:43 +0545

    Third Commit

---
+++ Command parser/Factory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_parser
+{
+    class Factory:Creator
+    {
+        public override Ishape getShape(string shapeType)
+        {
+
+
+            shapeType = shapeType.ToLower().Trim();
+            if (shapeType.Equals("line"))
+            {
+                return new Line();
+            }
+            else if (shapeType.Equals("rectangle"))
+            {
+                return new Rectangle();
+            }
+            else if (shapeType.Equals("circle"))
+            {
+                return new Circle();
+            }
+            else if (shapeType.Equals("triangle"))
+            {
+                return new Triangle();
+            }
+            else
+            {
+                //Exception Handling
+                System.ArgumentException argEx = new System.ArgumentException("Factory error: " + shapeType + " does not exist");
+                throw argEx;
+            }
+        }
+    }
+}
+}
---
+++ Command parser/Line.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_parser
+{
+    public class Line : Ishape
+    {
+      
+        public int x, y, toX, toY;
+
+
+
+       
+        public void draw(Graphics g)
+        {
+            try
+            {
+                Pen p = new Pen(Color.Black, 2);
+                g.DrawLine(p, x, y, toX, toY);
+            }
+            catch (Exception exception)
+            {
+
+                throw exception;
+            }
+        }
+       
+        public void set(params int[] list)
+        {
+            this.x = list[0];
+            this.y = list[1];
+            this.toX = list[2];
+            this.toY = list[3];
+        }
+    }
+}
